3b05db5b33238ff15b55eaa6ca922a678ac413a0
Diego <119649331+818diego@users.noreply.github.com>
2025-09-11 00:09:57 -0700

    feat: agregar soporte para Vercel Analytics y mejorar metadatos SEO en index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import PaquetesVipPage from './pages/PaquetesVipPage';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <Router>
+      <Analytics />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/paquetes-vip" element={<PaquetesVipPage />} />
