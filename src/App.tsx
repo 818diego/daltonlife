c7e5bf5a0953df284eda1a059203814ff182924f
Diego <119649331+818diego@users.noreply.github.com>
2025-09-11 00:12:26 -0700

    fix: se elimino codigo inesesario y cambio la ruta de analiticas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import PaquetesVipPage from './pages/PaquetesVipPage';
-import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <Router>
-      <Analytics />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/paquetes-vip" element={<PaquetesVipPage />} />
